719d5d2d77ef274b280fbd0ba61be0d98ca0cff9
charlene04 <charlesugbana04@gmail.com>
2020-02-24 20:15:14 +0100

    db issues

--- app.js
+++ app.js
@@ -18,15 +18,13 @@ var isLoggedIn = require("./middleware/isLoggedIn"),
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.DATABASEURL;
 MongoClient.connect(uri, function(err, db) {
-  test.equal(null, err);
-  test.ok(db != null);
+ 
 
   db.collection("replicaset_mongo_client_collection").update({a:1}, {b:1}, {upsert:true}, function(err, result) {
-    test.equal(null, err);
-    test.equal(1, result);
+   
 
     db.close();
-    test.done();
+   
   });
 });
 
