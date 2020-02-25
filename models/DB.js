efd2778902970ad610c134b480553170960991d4
charlene04 <charlesugbana04@gmail.com>
2020-02-25 03:09:58 +0100

    db issues

--- models/DB.js
+++ models/DB.js
@@ -5,13 +5,22 @@ console.log(url);
 var _db;
 
 module.exports = {
-
-  connectToServer: function( callback ) {
-    MongoClient.connect( url,  { useNewUrlParser: true, useUnifiedTopology: true }, function( err, client ) {
-      _db  = client.db('test_db');
+    connectToServer: async function main(){
+        let client;
+        try{
+           client = await MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+           _db = await client.db('tatiana');  
+        }
+        catch(err){ console.error(err); } // catch any mongo error here
+        finally{ client.close(); } // make sure to close your connection after
+       },
+       /*
+  connectToServer: async function( callback ) {
+    await MongoClient.connect( url,  { useNewUrlParser: true, useUnifiedTopology: true }, function(client) {
+      _db  = await client.db('tatiana');
       return callback( err );
     } );
-  },
+  },*/
 
   getDb: function() {
     return _db;
