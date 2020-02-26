be7311161f65b25359e97d358f1e80e765021e27
charlene04 <charlesugbana04@gmail.com>
2020-02-26 17:07:44 +0100

    db issues

--- models/DB.js
+++ models/DB.js
@@ -28,7 +28,7 @@ module.exports = {
         let client;
         try{
            _db = await mongoose.connect(url,  { useNewUrlParser: true });
-            
+        }  
     
         catch(err){ console.error(err); } // catch any mongo error here
         finally{ _db.close(); } // make sure to close your connection after
